e145e70f232e450475ee4df712b132aae64b1f20
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2019-02-07 14:20:44 +0000

    Update port to 8080

--- lib/origin_simulator/application.ex
+++ lib/origin_simulator/application.ex
@@ -8,7 +8,7 @@ defmodule OriginSimulator.Application do
       Plug.Adapters.Cowboy.child_spec(
         scheme: :http,
         plug: OriginSimulator,
-        options: [port: 8084, protocol_options: [max_keepalive: 5_000_000]]
+        options: [port: 8080, protocol_options: [max_keepalive: 5_000_000]]
       ),
       OriginSimulator.Simulation,
       OriginSimulator.Payload
