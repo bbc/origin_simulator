6af40b94998c259e5dc439aba6bc0d0b099ba485
Ettore Berardi <ettore.berardi@bbc.co.uk>
2019-02-28 17:54:45 +0000

    Adds a supervision tree.
    The new approach uses a supervisor tree where the Application starts Cowboy and
    a Supervisor with a :one_for_one strategy. The supervisor instead uses a
    :one_for_all strategy so that at any restart of either Payload or Simulation
    both the GenServers are restarted.
    
    Previously any single killed process would restart Cowboy as well, which wasn't a
    good idea..

---
+++ lib/origin_simulator/supervisor.ex
@@ -0,0 +1,21 @@
+defmodule OriginSimulator.Supervisor do
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    children = [
+      OriginSimulator.Simulation,
+      OriginSimulator.Payload
+    ]
+
+    opts = [
+      strategy: :one_for_all,
+      max_restarts: 30
+    ]
+    Supervisor.init(children, opts)
+  end
+end
