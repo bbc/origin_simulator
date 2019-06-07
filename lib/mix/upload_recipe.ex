01421445c938690ede93ffa245b59f4e5a60d5e7
James Bowers <james.bowers@bbc.co.uk>
2019-06-07 16:31:51 +0100

    mix task to upload recipes

---
+++ lib/mix/upload_recipe.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.UploadRecipe do
+  use Mix.Task
+
+  def run([host, recipe]) do
+    {:ok, _started} = Application.ensure_all_started(:httpoison)
+
+    recipe = get_json("./recipes/#{recipe}.json")
+
+    HTTPoison.post("#{host}/add_recipe", recipe)
+  end
+
+  def run([recipe]) do
+    run(["http://localhost:8080", recipe])
+  end
+
+  defp get_json(filename) do
+    File.read!(filename)
+  end
+end
