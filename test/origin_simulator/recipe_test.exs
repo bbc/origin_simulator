9aead70b27b27f7db887aa9b2c7c1619d93cb3b4
Ryan Wood <ryan.wood01@bbc.co.uk>
2019-09-27 12:55:32 +0100

    Add headers to the Recipe

--- test/origin_simulator/recipe_test.exs
+++ test/origin_simulator/recipe_test.exs
@@ -6,4 +6,8 @@ defmodule OriginSimulator.RecipeTest do
   test "Parse valid JSON" do
     assert Recipe.parse({:ok, ~s[{"random_content": "123kb"}], nil}) ==  %OriginSimulator.Recipe{random_content: "123kb"}
   end
+
+  test "Given some headers it adds them to the recipe" do
+    assert Recipe.parse({:ok, ~s[{"headers": {"host": "www.example.com"}}], nil}) == %OriginSimulator.Recipe{headers: %{"host" => "www.example.com"}}
+  end
 end
